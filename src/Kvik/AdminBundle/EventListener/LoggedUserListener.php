5a92765ecffe6fb2146043f3f44b90b87fd9ee9c
Lionel <lionel@permisapoints.fr>
2018-05-17 18:26:49 +0200

    Suppression du bundle par defaut : AppBunlde

---
+++ src/Kvik/AdminBundle/EventListener/LoggedUserListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Kvik\AdminBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class LoggedUserListener
+{
+    private $router;
+    private $authChecker;
+
+    public function __construct(RouterInterface $router, AuthorizationCheckerInterface $authChecker)
+    {
+        $this->router = $router;
+        $this->authChecker = $authChecker;
+    }
+
+    /**
+     * Redirect user to homepage if tryes to access in anonymously path
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        $path = $request->getPathInfo();
+        if ($this->authChecker->isGranted('IS_AUTHENTICATED_REMEMBERED') && $this->isAnonymouslyPath($path)) {
+            $response = new RedirectResponse($this->router->generate('kvik_admin_index'));
+            $event->setResponse($response);
+        }
+    }
+
+    /**
+     * Check if $path is an anonymously path
+     * @param string $path
+     * @return bool
+     */
+    private function isAnonymouslyPath($path)
+    {
+        return preg_match('/\/login|\/register|\/resetting/', $path) ? true : false;
+    }
+}
